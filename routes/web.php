ed5cd28d2f39ddab3f2f2ba0777708bda325bf2b
CHriss <christophervistal25@gmail.com>
2019-12-17 21:42:52 +0800

    Add auth middleware for admin's page

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,5 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
 Route::get('logout', 'Auth\LoginController@logout');
