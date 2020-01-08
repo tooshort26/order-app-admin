0fa2beb2a9374f1394654370a4bd552857fb8938
CHriss <christophervistal25@gmail.com>
2020-01-08 14:16:11 +0800

    change the code in seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -11,6 +12,14 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory('App\User')->create();
+    	User::create([
+		        'name' => 'Adminstrator Mai Place',
+		        'email' => 'admin@yahoo.com',
+		        'email_verified_at' => now(),
+		        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+		        'remember_token' => Str::random(10),
+		        'mobile_no' => '09193693499'
+	    ]);
+    	 
     }
 }
